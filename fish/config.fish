a99770568009c67dbda7cc040ee06b1dfb1e7983
Dan <daniel@fail.ie>
2023-09-20 17:39:50 +0100

    Merge current fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,52 @@
-set ALIASES "$HOME/.dotfiles/aliases.sh"
-set ENV "$HOME/.env"
+# Hide welcome message
+set fish_greeting
+set VIRTUAL_ENV_DISABLE_PROMPT "1"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+## Export variable need for qt-theme
+if type "qtile" >> /dev/null 2>&1
+   set -x QT_QPA_PLATFORMTHEME "qt5ct"
+end
+
+# Set settings for https://github.com/franciscolourenco/done
+set -U __done_min_cmd_duration 10000
+set -U __done_notification_urgency_level low
+
+
+## Environment setup
+# Apply .profile: use this to put fish compatible .profile stuff in
+if test -f ~/.fish_profile
+  source ~/.fish_profile
+end
+
+# Add ~/.local/bin to PATH
+if test -d ~/.local/bin
+    if not contains -- ~/.local/bin $PATH
+        set -p PATH ~/.local/bin
+    end
+end
+
+## Advanced command-not-found hook
+source /usr/share/doc/find-the-command/ftc.fish
 
 ## Starship prompt
 if status --is-interactive
    starship init fish | source
 end
 
-if test -e $ENV
-    source $ENV
-end
+set dotfiles "$HOME/.dotfiles/aliases.sh" "$HOME/.config/fish/functions/functions.fish" "$HOME/.env"
 
-if test -e $ALIASES
-    source $ALIASES
+for df in $dotfiles
+  if test -e $df
+    source $df
+  end
 end
 
 if test (uname) = 'Darwin'
   source "$HOME/.dotfiles/macos.fish"
+end
+
+# Run fastfetch if session is interactive
+if status --is-interactive && type -q fastfetch
+  fastfetch --load-config neofetch
 end
